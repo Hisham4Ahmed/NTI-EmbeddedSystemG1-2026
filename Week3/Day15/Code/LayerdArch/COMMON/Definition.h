445397d2c61383af40fe6cdeacf1bdf1f08026c8
Hesham Ahmed <Hisham4Ahmed@gmail.com>
2026-07-23 10:55:31 +0300

    EXTI

---
+++ Week3/Day15/Code/LayerdArch/COMMON/Definition.h
@@ -0,0 +1,19 @@
+#ifndef _DEFINITION_H
+#define _DEFINITION_H 
+
+#define Input   0 
+#define Output  1 
+
+#define Low     0 
+#define High    1 
+
+#define Disable 0 
+#define Enable  1
+
+#define Off     0 
+#define On      1 
+
+#define Null (void*)0x00 
+
+
+#endif 
